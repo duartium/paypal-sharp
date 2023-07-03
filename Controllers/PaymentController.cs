1422e9fb9d7448a0f4fe8c6f001575159452aaa8
Byron Duarte <byronduarte95@gmail.com>
2023-07-03 18:08:02 -0500

    endpoint authenticate

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper.Internal;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using paypal_sharp.Core.Models.Response;
+using paypal_sharp.Core.Services.Http;
+using System.Text.Json;
 
 namespace paypal_sharp.Controllers
 {
@@ -12,6 +16,28 @@ namespace paypal_sharp.Controllers
 
         }
 
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate()
+        {
+            AuthResponse responseContent = new AuthResponse();
+            string credentials = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(""));
+            var authorizathion = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", credentials);
+
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "client_credentials")
+            });
+
+            var response = await RequestHandler.Post("https://api-m.sandbox.paypal.com/v1/oauth2/token", content, null, authorizathion);
+            string contentResponse = await response.Content.ReadAsStringAsync();
+
+            if (!string.IsNullOrWhiteSpace(contentResponse))
+                responseContent = JsonSerializer.Deserialize<AuthResponse>(contentResponse);
+
+
+            var statusCode = (int)response.StatusCode;
+            return new ObjectResult(JsonSerializer.Serialize(responseContent)) { StatusCode = statusCode };
+        }
 
     }
 }
---
+++ Core/Services/Http/RequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http.Headers;
+
+namespace paypal_sharp.Core.Services.Http
+{
+    public class RequestHandler
+    {
+        public static async Task<HttpResponseMessage> Post(string url, HttpContent data, Dictionary<string, string> headers = null, AuthenticationHeaderValue authentication = null)
+        {
+            HttpResponseMessage response = null;
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    if (authentication != null)
+                    {
+                        client.DefaultRequestHeaders.Authorization = authentication;
+                    }
+
+                    if (headers != null)
+                        headers.ToList().ForEach(header => client.DefaultRequestHeaders.Add(header.Key, header.Value));
+
+                    response = await client.PostAsync(url, data);
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al realizar la petición POST: " + ex.Message);
+                }
+                return response;
+            }
+        }
+    }
+}
