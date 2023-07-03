566483cb7de8ecbf253ff462dc68b12273235fef
Byron Duarte <byronduarte95@gmail.com>
2023-07-03 17:41:21 -0500

    initial commit

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace paypal_sharp.Controllers
+{
+    [Route("api/payment")]
+    [ApiController]
+    public class PaymentController : BaseApiController<PaymentController>
+    {
+        public PaymentController()
+        {
+
+        }
+
+
+    }
+}
